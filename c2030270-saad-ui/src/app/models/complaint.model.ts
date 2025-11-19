e24bdfd51723fc6896909e3818f38f1c6bb6c789
donkeytoadd <jordan.aaron.hanson@gmail.com>
2025-11-19 15:01:57 +0000

    remove old ui, had to restart project

--- c2030270-saad-ui/src/app/models/complaint.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Complaint {
-  id: number;
-  subject: string;
-  description?: string;
-  status: 'Open' | 'InProgress' | 'Resolved' | 'Closed' | string;
-  createdDate: string;
-}
--- c2030270-saad-ui/src/app/models/consumer.model.ts
+++
@@ -1,6 +0,0 @@
-export interface Consumer {
-  id: number;
-  name: string;
-  email?: string;
-  phone?: string;
-}
