c7946ae32210fc2eae453a18e05f5303bad36d07
donkeytoadd <jordan.aaron.hanson@gmail.com>
2025-11-29 13:29:10 +0000

    install Scrutor package to help manage dependency injection within project + cleanup program.cs file

--- c2030270-saad/Program.cs
+++ c2030270-saad/Program.cs
@@ -1,25 +1,4 @@
-using c2030270_saad.Business.Creators.Complaint;
-using c2030270_saad.Business.Creators.Complaint.Interfaces;
-using c2030270_saad.Business.Creators.Tenant;
-using c2030270_saad.Business.Creators.Tenant.Interfaces;
-using c2030270_saad.Business.Getters.Complaint;
-using c2030270_saad.Business.Getters.Complaint.Interfaces;
-using c2030270_saad.Business.Getters.Staff;
-using c2030270_saad.Business.Getters.Staff.Interfaces;
-using c2030270_saad.Business.Getters.Tenant;
-using c2030270_saad.Business.Getters.Tenant.Interfaces;
-using c2030270_saad.Business.Updaters.Complaint;
-using c2030270_saad.Business.Updaters.Complaint.Interfaces;
 using c2030270_saad.Data;
-using c2030270_saad.Data.Queries;
-using c2030270_saad.Data.Queries.Complaint;
-using c2030270_saad.Data.Queries.Complaint.Interfaces;
-using c2030270_saad.Data.Queries.Consumer;
-using c2030270_saad.Data.Queries.Consumer.Interfaces;
-using c2030270_saad.Data.Queries.Staff;
-using c2030270_saad.Data.Queries.Staff.Interfaces;
-using c2030270_saad.Data.Queries.Tenant;
-using c2030270_saad.Data.Queries.Tenant.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
@@ -31,22 +10,17 @@ builder.Services.AddHealthChecks();
 builder.Services.AddDbContextFactory<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddScoped<IComplaintGetter, ComplaintGetter>();
-builder.Services.AddScoped<IComplaintCreator, ComplaintCreator>();
-builder.Services.AddScoped<IComplaintStatusUpdater, ComplaintStatusUpdater>();
-builder.Services.AddScoped<ITenantCreator, TenantCreator>();
-builder.Services.AddScoped<ITenantGetter, TenantGetter>();
-builder.Services.AddScoped<IGetComplaintByIdQuery, GetComplaintByIdQuery>();
-builder.Services.AddScoped<IGetAllComplaintsByConsumerIdQuery, GetComplaintsByConsumerIdQuery>();
-builder.Services.AddScoped<IGetAllComplaintsByTenantIdQuery, GetComplaintsByTenantIdQuery>();
-builder.Services.AddScoped<ICreateComplaintQuery, CreateComplaintQuery>();
-builder.Services.AddScoped<IGetConsumerByConsumerIdQuery, GetConsumerByConsumerIdQuery>();
-builder.Services.AddScoped<IUpdateComplaintStatusQuery, UpdateComplaintStatusQuery>();
-builder.Services.AddScoped<ICreateTenantQuery, CreateTenantQuery>();
-builder.Services.AddScoped<IGetAllTenantsQuery, GetAllTenantsQuery>();
-builder.Services.AddScoped<IGetTenantByTenantIdQuery, GetTenantByTenantIdQuery>();
-builder.Services.AddScoped<IStaffGetter, StaffGetter>();
-builder.Services.AddScoped<IGetStaffByStaffIdQuery, GetStaffByStaffIdQuery>();
+builder.Services.Scan(scan => scan
+    .FromAssemblies(typeof(Program).Assembly)
+    .AddClasses(c => c.Where(t =>
+        t.Name.EndsWith("Getter") ||
+        t.Name.EndsWith("Creator") ||
+        t.Name.EndsWith("Updater") ||
+        t.Name.EndsWith("Query")
+    ))
+    .AsImplementedInterfaces()
+    .WithScopedLifetime()
+);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
@@ -84,9 +58,6 @@ app.UseSwaggerUI(c =>
     c.RoutePrefix = "swagger";
 });
 
-app.UseStaticFiles();
-
-app.UseRouting();
 app.UseAuthorization();
 app.MapControllers();
 app.MapHealthChecks("/api/health");
